868aaa4b6c56475a419097f2134b41925c100b68
yulianisyaradelola30 <yulianisyaradelola30>
2020-01-12 11:47:07 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from view.view_nilai import cari,cetak,header,notoption
+from view.input_nilai import inputan,ubah,hapus
+header()
+while True:
+
+    c = input("\nPilih Opsi: ")
+
+    # EXIT PROGRAM
+    if c.lower() == 'k':
+        print(".:TERIMAKASIH TELAH MENGGUNAKAN PROGRAM INI:.\n"
+              "               yulian isyara                 \n"
+              "                 TI.19.D.2.                 ")
+        ext = input("\nPress ENTER to exit")
+        if ext == '':
+            break
+        else:
+            break
+
+    # PRINT DATA
+    elif c.lower() == 'l':
+        cetak()
+
+    # MENAMBAH DATA
+    elif c.lower() == 't':
+        inputan()
+
+    # UBAH DATA
+    elif c.lower() == 'u':
+        ubah()
+
+    # MENCARI DATA
+    elif c.lower() == 'c':
+        cari()
+
+    # HAPUS DATA
+    elif c.lower() == 'h':
+        hapus()
+
+    else:
+        notoption()
